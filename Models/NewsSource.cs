ad398c0dc008d4626e2de50393ec054522165b72
Henry James <haz@hazjames.com>
2021-04-11 11:56:29 +0100

    Added models and added list of news sources

---
+++ Models/NewsSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NewsFeedApi.Models
+{
+    public class NewsSource
+    {
+        public string Name { get; }
+        public Uri Url { get; }
+
+        public NewsSource(string name, string url)
+        {
+            Name = name;
+            Url = new Uri(url);
+        }
+    }
+}
---
+++ Models/NewsStory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NewsFeedApi.Models
+{
+    public class NewsStory
+    {
+        public string Title { get; }
+        public Uri Link { get; }
+        public string Description { get; }
+        public string Source { get; }
+        public DateTime PublishedDate { get; }
+        public Uri Thumbnail { get; }
+
+        public NewsStory(string title, string link, string description, string source, DateTime publishedDate, string thumbnail)
+        {
+            Title = title;
+            Link = new Uri(link);
+            Description = description;
+            Source = source;
+            PublishedDate = publishedDate;
+            Thumbnail = new Uri(thumbnail);
+        }
+    }
+}
